1cf0ae814f65defe7c09a8f379fc3b7746d7100d
Edward Weng <edweng@gmail.com>
2012-07-25 10:21:42 -0400

    Refactored. Extracted repetitive code into individual methods. Made use of tap to build hashes conditionally.

--- lib/scoop.rb
+++ lib/scoop.rb
@@ -11,19 +11,23 @@ class Scoop
     @default_api_url = url
   end
 
+  def parse_http_response(response)
+    status = response.status
+    response = JSON.parse(response.body)
+    [status, response] 
+  end
+
   def create_participant(user_id, link)
     participant = { user_id: user_id, link: link }
     response = connect.post "participants.json", participant
-    status = response.status
-    response = JSON.parse(response.body)
+    status, response = parse_http_response(response)
 
-    result = { status: status, participant: response }
+    { status: status, participant: response }
   end
 
   def get_seed(seed_id)
     response = connect.get "seeds/#{seed_id}.json"
-    status = response.status
-    response = JSON.parse(response.body)
+    status, response = parse_http_response(response)
 
     unless status == 200
       { status: status, error: response["error"]}
@@ -34,70 +38,63 @@ class Scoop
 
   def get_all_seeds
     response = connect.get "seeds.json"
-    status = response.status
-    response = JSON.parse(response.body)
+    status, response = parse_http_response(response)
+
     seeds = response.collect do |seed|
       build_seed_without_status(seed)
     end
-    { status: status,
-      seeds: seeds }
+    { status: status, seeds: seeds }
   end
 
-  def create_seed(user_id, amount_cents)
-    seed = { user_id: user_id, amount_cents: amount_cents }
+  def create_seed(user_id, amount_cents, link = nil)
+    seed = {}.tap do |h|
+      h[:user_id] = user_id
+      h[:amount_cents] = amount_cents
+      h[:link] = link if link
+    end
+    
     response = connect.post "seeds.json", seed
-    status = response.status
-    response = JSON.parse(response.body)
+    status, response = parse_http_response(response)
+
     build_seed_with_status(response, status)
   end
 
   def reseed_seed(user_id, link, amount_cents)
-    reseed = { user_id: user_id, link: link, amount_cents: amount_cents }
-    response = connect.post "seeds.json", reseed
-    status = response.status
-    response = JSON.parse(response.body)
-    build_seed_with_status(response, status)
+    create_seed(user_id, amount_cents, link)
   end
 
   def get_tree(seed_id)
     response = connect.get "trees/#{seed_id}.json"
-    status = response.status
-    response = JSON.parse(response.body)
-    response[:status] = status
-    response
+    status, response = parse_http_response(response)
+
+    response.merge(:status => status)
   end
 
   private
 
   def build_seed_with_status(response, status)
+    result = { status: status, id: response["id"].to_i, link: response["link"]}
     if response["donation"]
-      donation = response["donation"]
-      result = { status: status,
-        id: response["id"].to_i,
+      result.merge!({ 
         user_id: response["user_id"].to_i,
-        link: response["link"],
         participants: response["child_count"],
-        donation: { amount_cents: donation["amount_cents"],
-                    payout_cents: donation["payout_cents"],
-                    total_donated: response["total_donated"] } }
-        
-    else
-      result = { status: status, id: response["id"].to_i, link: response["link"]}
+        donation: get_donations_hash(response) })
     end
     result
   end
 
-  def build_seed_without_status(response)
-    if response["donation"]
-      donation = response["donation"]
-      result = {
-        id: response["id"].to_i,
-        link: response["link"],
-        donation: { amount_cents: donation["amount_cents"],
-                    payout_cents: donation["payout_cents"] } }
-    else
-      result = { id: response["id"].to_i, link: response["link"]}
+  def get_donations_hash(response)
+    donation = response["donation"]
+    {}.tap do |h|
+      h[:amount_cents] = donation["amount_cents"]
+      h[:payout_cents] = donation["payout_cents"]
+      h[:total_donated] = response["total_donated"] if response["total_donated"]
     end
+  end
+
+  def build_seed_without_status(response)
+    result = { id: response["id"].to_i, link: response["link"]}
+    result.merge!({ donation: get_donations_hash(response) }) if response["donation"]
     result
   end
 end
